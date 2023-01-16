3056d840e9d6bf8e61ed4880a2d06dc46ad7fc84
JakeDEsposito <97902257+JakeDEsposito@users.noreply.github.com>
2023-01-16 06:57:15 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { lazy, Suspense } from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { HashRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Choice from './Choice'
 
